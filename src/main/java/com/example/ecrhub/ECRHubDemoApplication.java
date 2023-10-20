d974c05e75019ed4ef1442a39ca9bca06ea8e022
yanzixiong <yanzixiong@wiseasy.com>
2023-10-20 09:17:46 +0800

    打包配置添加

--- src/main/java/com/example/ecrhub/ECRHubDemoApplication.java
+++ src/main/java/com/example/ecrhub/ECRHubDemoApplication.java
@@ -8,14 +8,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ECRHubDemoApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
         SceneManager.getInstance().setStage(stage);
         SceneManager.getInstance().loadScene("home", "/fxml/home.fxml");
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ECRHubDemoApplication.class.getResource("/fxml/home.fxml"));
 //        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/shopping.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("ECR-Hub Demo");
---
+++ src/main/java/com/example/ecrhub/ExecuteMain.java
@@ -0,0 +1,13 @@
+package com.example.ecrhub;
+
+/**
+ * @author: yanzx
+ * @date: 2023/10/20 09:16
+ * @description:
+ */
+public class ExecuteMain {
+
+    public static void main(String[] args) {
+        ECRHubDemoApplication.main(args);
+    }
+}
